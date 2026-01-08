ccaee9bcb624dd5beb527c98c3987dacab38c919
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-01-08 13:57:16 +0100

    refactor parameter reconciliation towards a more flodymic implementation

--- remind_mfa/common/parameter_reconciliation.py
+++ remind_mfa/common/parameter_reconciliation.py
@@ -1,48 +1,44 @@
 import numpy as np
-from typing import List, Callable, Dict, Tuple
+from typing import Callable, Tuple, Optional
 import flodym as fd
 import logging
 
-from remind_mfa.common.common_definition import RemindMFAParameterDefinition
-
+from copy import deepcopy
 class ParameterReconciliation:
     """Parameter reconciliation of top-down and bottom-up models."""
+    # TODO inherit from separate helper class
+    # TODO pydantic?
 
-    def __init__(self, cfg, prms, dims):
-        self.cfg = cfg
+    def __init__(self, prms: dict[str, fd.Parameter], dims: fd.DimensionSet):
 
-        self._time = 2020
+        self._year_of_reconciliation = 2020
         self._reduced_stock_type = fd.Dimension(name="Reduced Stock Type", letter="u", items=["Res", "Com"])
         # save computation time
         self._no_correction_dim_letters = ('t', 'h') # instead of df/dx, now calculating df/dd 
         # TODO allow option to bring in knowledge that parameters and stock dimensions often do not interact
         # TODO set and skip over known sensitivity parameters
 
-
         self.prepare_dims(dims)
         self.prepare_prms(prms)
-
-        self.bu = self.calc_bottom_up_stock(self.prms)
-        self.td = self.calc_top_down_stock(self.prms)
-
-        self.pre_compute_sensitivity(self.calc_bottom_up_stock, self.bu, denominator=True)
-        self.pre_compute_sensitivity(self.calc_top_down_stock, self.td)
-
-        self.pre_compute_lambda()
-        self.calc_corrections()
-
-        print(self.system_model(self.prms))
         self.correct_parameters()
-        print(self.system_model(self.prms))
 
-
-        self.f0 = self.system_model(self.prms)
+    def prepare_dims(self, dims: fd.DimensionSet):
+        self.dims = dims.replace('s', self._reduced_stock_type)
+    
+    def prepare_prms(self, prms: dict[str, fd.Parameter]):
+        self.prms = {}
+        self.prms_adj_dims = {}
+        for key, val in prms.items():
+            if "s" in val.dims.letters:
+                val = val[{"s": self._reduced_stock_type}]
+            self.prms[key] = val
+            self.prms_adj_dims[key] = self.extract_wanted_dims(val.dims)
 
     def letters_to_drop(self, dims: fd.DimensionSet) -> Tuple:
         drop_letters = tuple(dl for dl in self._no_correction_dim_letters if dl in dims.letters)
         return drop_letters
 
-    def extract_wanted_dims(self, dims: fd.DimensionSet, drop_letters: Tuple = None) -> fd.DimensionSet:
+    def extract_wanted_dims(self, dims: fd.DimensionSet, drop_letters: Optional[Tuple] = None) -> fd.DimensionSet:
         """Extract relevant dimensions."""
         if not drop_letters:
             drop_letters = self.letters_to_drop(dims)
@@ -50,44 +46,176 @@ class ParameterReconciliation:
         for drop_letter in drop_letters:
             new_dims = new_dims.drop(drop_letter)
         return new_dims
+
+    def correct_parameters(self):
+        self.original_prms = deepcopy(self.prms)
+
+        self.td = self.calc_top_down_stock(self.prms)
+        self.bu = self.calc_bottom_up_stock(self.prms)
+
+        # Assume (without loss of generality) target function of td/bu = 1s
+        self.pre_compute_sensitivity(self.calc_top_down_stock, self.td)
+        self.pre_compute_sensitivity(self.calc_bottom_up_stock, self.bu, denominator=True)
         
-    def prepare_dims(self, dims: fd.DimensionSet):
-        self.dims = dims.replace('s', self._reduced_stock_type)
+        self.pre_compute_lambda()
+        self.calc_corrections()
 
-    def prepare_prms(self, prms: Dict[str, fd.FlodymArray]):
+        # TODO see if I really want to do the mulitplication here, or rather in parameter extrapolation?
+        self.updated_prms = self.prms
+        for prm_name, c in self.correction_factors.items():
+            self.updated_prms[prm_name] = self.prms[prm_name] * c
 
-        self.prms = {}
-        for key, val in prms.items():
-            self.prms[key] = val[{"s": self._reduced_stock_type}] if "s" in val.dims.letters else val
+        print("done")
 
-        self.bu_params = self.get_relevant_parameters(self.calc_bottom_up_stock, self.prms)
-        self.td_params = self.get_relevant_parameters(self.calc_top_down_stock, self.prms)
+    def calc_bottom_up_stock(self, prm: dict[str, fd.FlodymArray]):
+        stk = prm["concrete_building_mi"] * prm["building_split"] * prm["floorspace"]
+
+        stk = stk[{"t": self._year_of_reconciliation}]
+
+        # build up new stock where function (f) and stock type (s) are merged into reduced stock type (u)
+        new_stk = fd.FlodymArray(dims=stk.dims.drop("f"))
+        new_stk[{'u': 'Res'}] = stk[{'f': 'RS', 'u': "Res"}] + stk[{'f': 'RM', 'u': "Res"}]
+        new_stk[{'u': 'Com'}] = stk[{'f': 'Com', 'u': "Com"}]
+        new_stk = new_stk.sum_over('b')
+        return new_stk
+    
+    def calc_top_down_stock(self, prm: dict[str, fd.FlodymArray]):
+        cement_consumption = (
+            (1 - prm["cement_losses"])
+            * (prm["cement_production"] - prm["cement_trade"])
+            * prm["stock_type_split"]
+        )
+
+        # cement stock
+        stk = fd.InflowDrivenDSM(
+            dims=self.dims[cement_consumption.dims.letters],
+            lifetime_model=fd.LogNormalLifetime,
+            time_letter="h",
+        )
+        stk.inflow[...] = cement_consumption
+        stk.lifetime_model.set_prms(
+            mean=prm["use_lifetime_mean"],
+            std=prm["use_lifetime_rel_std"] * prm["use_lifetime_mean"],
+        )
+        stk.compute()
+
+        # product stock
+        cement_ratio = (
+            prm["product_cement_content"] / prm["product_density"]
+        )
+        stk = stk.stock * (
+            prm["product_material_split"]
+            * prm["product_material_application_transform"]
+            * prm["product_application_split"]
+            / cement_ratio
+        )
+        
+        # reduce redudant dimensions
+        stk = stk[{"h": self._year_of_reconciliation}]
+        stk = stk[{"m": "concrete"}]
+        stk = stk.sum_over("a")
+        return stk
+    
+    def pre_compute_sensitivity(self, f: Callable[[dict[str, fd.FlodymArray]], fd.FlodymArray], f0: fd.FlodymArray, denominator: bool = False):
+        """
+        Pre-compute sensitivity matrices for parameters used in the given model function.
+        Pre-existing sensitivities are added to newly computed ones.
+        """
+        relevant_params = self.get_relevant_parameters(f, self.prms)
+
+        # Initialize S_matrices dictionary if it doesn't exist
+        if not hasattr(self, "S_matrices"):
+            self.S_matrices = {}
+    
+        for prm_name in relevant_params:
+            logging.info(f"Calculating sensitivity for parameter: {prm_name}")
+            S_mat = self.calc_sensitivity(f, f0, prm_name, denominator=denominator)
+            if prm_name in self.S_matrices:
+                # TODO check if that makes sense
+                logging.info(f"Sensitivity for parameter {prm_name} already exists; summing matrices.")
+                self.S_matrices[prm_name] = self.S_matrices[prm_name] + S_mat
+            else:
+                self.S_matrices[prm_name] = S_mat
+
+    @staticmethod
+    def get_relevant_parameters(model_func: Callable, prms: dict[str, fd.Parameter]) -> set:
+        """
+        Runs a model once to spy on which parameters are used.
+        """
+        # Wrap the parameters in a tracking dict
+        spy_prms = DependencyTracker(prms)
+        
+        # Run the model
+        _ = model_func(spy_prms)
+
+        return spy_prms.accessed_keys
+    
+    def calc_sensitivity(self, f: Callable[[dict[str, fd.FlodymArray]], fd.FlodymArray], f0: fd.FlodymArray, prm_name: str, denominator: bool = False):
+        J = self.calc_jacobian(f, f0, prm_name)
+        
+        # scaling by f0 for logarithmic sensitivity
+        f0_flat = self.flatten_fd_to_np(f0)
+        f0_col = f0_flat[:, np.newaxis]
+
+        S = J / f0_col
+        
+        if denominator:
+            return -S
+        
+        return S
+    
+    def calc_jacobian(self, f: Callable[[dict[str, fd.FlodymArray]], fd.FlodymArray], f0: fd.FlodymArray, prm_name: str, epsilon=1e-5):
+
+        # TODO why is dims_to_adj not recognized as dimensionset?
+        prm = self.prms[prm_name]
+        original_prm = self.original_prms[prm_name]
+        dims_to_adj = self.prms_adj_dims[prm_name]
+        
+        output_dim = f0.size
+        param_dim = dims_to_adj.total_size        
+        J = np.zeros((output_dim, param_dim))
+
+        # TODO move looping into separate helper function
+        # enumerate indeed works the same as np.flatten
+        import itertools
+        all_dim_items = [d.items for d in dims_to_adj]
+        for flat_idx, dim_element in enumerate(itertools.product(*all_dim_items)):
+            slicer = dict(zip(dims_to_adj.letters, dim_element))
+            val = original_prm[slicer]
+
+            # Perform perturbation
+            # zero values are not corrected
+            prm[slicer] = val * (1 + epsilon)
+            f_perturbed = f(self.prms)
+            J[:, flat_idx] = self.flatten_fd_to_np(f_perturbed - f0) / epsilon
+            
+            # Restore original value
+            prm[slicer] = val
+
+        return J
     
     def flatten_fd_to_np(self, arr: fd.FlodymArray) -> np.ndarray:
         """Flatten a FlodymArray into a 1D numpy array."""
         return arr.values.flatten()
     
-    def reshape_np_to_fd(self, flat_arr: np.ndarray, template: fd.FlodymArray) -> fd.FlodymArray:
-        """Reshape a 1D numpy array back into a FlodymArray with the same shape as the template."""
-        if flat_arr.size != template.values.size:
-            raise ValueError("Size of flat array does not match size of template.")
-        reshaped_values = flat_arr.reshape(template.values.shape)
-        return fd.FlodymArray(dims=template.dims, values=reshaped_values)
+    def pre_compute_lambda(self):
+        """Solve Aλ = b for λ."""
 
-    def _get_parameter_space(self, prm_name: str) -> Tuple[fd.FlodymArray, Tuple[str, ...], fd.DimensionSet]:
-        prm = self.prms[prm_name]
-        drop_letters = self.letters_to_drop(prm.dims)
-        correction_dims = self.extract_wanted_dims(prm.dims, drop_letters)
-        return prm, drop_letters, correction_dims
+        b = self.flatten_fd_to_np(self.td.apply(np.log) - self.bu.apply(np.log))
+        D = b.size
+        A = np.zeros((D, D))
+        for prm_name, S in self.S_matrices.items():
+            var_vec = self.get_sigma(prm_name)
+            S_weighted = S * var_vec[np.newaxis, :]
+            A += S_weighted @S.T
 
-    def _build_template(self, dims: fd.DimensionSet) -> fd.FlodymArray:
-        values = np.zeros(dims.total_size, dtype=float).reshape(dims.shape)
-        return fd.FlodymArray(dims=dims, values=values)
+        # solve for lambda
+        self.lmda = np.linalg.solve(A, -b)
 
-    def _reshape_vector_to_parameter(self, prm_name: str, flat_arr: np.ndarray) -> fd.FlodymArray:
-        _, _, correction_dims = self._get_parameter_space(prm_name)
-        template = self._build_template(correction_dims)
-        return self.reshape_np_to_fd(flat_arr, template)
+    def get_sigma(self, prm_name: str) -> np.ndarray:
+        rel_std = self.rel_std(prm_name)
+        sigma = self.flatten_fd_to_np(rel_std) ** 2
+        return sigma
     
     def rel_std(self, prm_name: str) -> fd.FlodymArray:
         """
@@ -117,184 +245,36 @@ class ParameterReconciliation:
         
         if isinstance(out, (float, int)):
             out = fd.FlodymArray(dims=fd.DimensionSet(dim_list=[]), values=np.array(out))
-        if not isinstance(out, fd.FlodymArray):
-            raise ValueError(f"Relative standard deviation for parameter {prm_name} not defined.")
         
-        output_dims = self.extract_wanted_dims(self.prms[prm_name].dims)
-        out = out.cast_to(output_dims)
+        out = out.cast_to(self.prms_adj_dims[prm_name])
         return out
-    
-    def get_sigma(self, prm_name: str) -> np.ndarray:
-        """Get the standard deviation of a parameter."""
-        rel_std = self.rel_std(prm_name)
-        sigma = self.flatten_fd_to_np(rel_std) ** 2
-        return sigma
-    
-    @staticmethod
-    def get_relevant_parameters(model_func: Callable, prm: List[RemindMFAParameterDefinition]) -> set:
-        """
-        Runs a model once to spy on which parameters are used.
-        """
-        # Wrap the parameters in a tracking dict
-        spy_prm = DependencyTracker(prm)
-        
-        # Run the model
-        _ = model_func(spy_prm)
-
-        return spy_prm.accessed_keys
-
-    def calc_bottom_up_stock(self, prm: list[fd.FlodymArray]):
-        stk = prm["concrete_building_mi"] * prm["building_split"] * prm["floorspace"]
-
-        stk = stk[{"t": self._time}]
-
-        new_dimletters = tuple(set(stk.dims.letters) - set('f'))
-        new_stk = fd.FlodymArray(dims=stk.dims[new_dimletters])
-        new_stk[{'u': 'Res'}] = stk[{'f': 'RS', 'u': "Res"}] + stk[{'f': 'RM', 'u': "Res"}]
-        new_stk[{'u': 'Com'}] = stk[{'f': 'Com', 'u': "Com"}]
-        new_stk = new_stk.sum_over('b')
-        return new_stk
-    
-    def calc_top_down_stock(self, prm: list[fd.FlodymArray]):
-        cement_consumption = (
-            (1 - prm["cement_losses"])
-            * (prm["cement_production"] - prm["cement_trade"])
-            * prm["stock_type_split"]
-        )
-
-        stk = fd.InflowDrivenDSM(
-            dims=self.dims[cement_consumption.dims.letters],
-            lifetime_model=fd.LogNormalLifetime,
-            time_letter="h",
-        )
-        # cement stock
-        stk
-        stk.inflow[...] = cement_consumption
-        stk.lifetime_model.set_prms(
-            mean=prm["use_lifetime_mean"],
-            std=prm["use_lifetime_rel_std"] * prm["use_lifetime_mean"],
-        )
-        stk.compute()
-
-        # product stock
-        cement_ratio = (
-            prm["product_cement_content"] / prm["product_density"]
-        )
-        stk = stk.stock * (
-            prm["product_material_split"]
-            * prm["product_material_application_transform"]
-            * prm["product_application_split"]
-            / cement_ratio
-        )
-
-        stk = stk[{"h": self._time}]
-        stk = stk[{"m": "concrete"}]
-        stk = stk.sum_over("a")
-        return stk
-    
-    def system_model(self, prm: List[fd.FlodymArray]):
-        return self.calc_top_down_stock(prm) / self.calc_bottom_up_stock(prm)
-
-    def pre_compute_sensitivity(self, f: Callable, f0, denominator: bool = False):
-        """Pre-compute sensitivity matrices for parameters used in the given model function."""
-        relevant_params = self.get_relevant_parameters(f, self.prms)
-        if not hasattr(self, "S_matrices") or not self.S_matrices:
-            self.S_matrices = {}
-    
-        for prm_name in relevant_params:
-            logging.info(f"Calculating sensitivity for parameter: {prm_name}")
-            if prm_name in self.S_matrices:
-                # TODO I can just sum the S matrices: self.S_matrices[prm_name] += S_mat
-                raise ValueError(f"Sensitivity for parameter {prm_name} already computed.",
-                                 "Parameters present in both bottom-up and top-down models currently not supported.")
-            S_mat = self.calc_sensitivity(f, f0, prm_name, denominator=denominator)
-            self.S_matrices[prm_name] = S_mat
-
-    def calc_sensitivity(self, f: Callable, f0: fd.FlodymArray, prm_name: str, denominator: bool = False):
-        J = self.calc_jacobian(f, f0, prm_name)
-        
-        # scaling by f0 for logarithmic sensitivity
-        f0_flat = self.flatten_fd_to_np(f0)
-        f0_col = f0_flat[:, np.newaxis]
-
-        S = J / f0_col
-        
-        if denominator:
-            return -S
-        
-        return S
-    
-    def calc_jacobian(self, f: Callable, f0: np.ndarray, prm_name: str, epsilon=1e-5):
-        """
-        Use finite differences to calculate the gradient of a function with respect to one parameter.
-        Expects that f0 is flat and f returns a flat array.
-        """
-        prm, drop_letters, correction_dims = self._get_parameter_space(prm_name)
-        
-        output_dim = f0.size
-        param_dim = correction_dims.total_size
-        J = np.zeros((output_dim, param_dim))
-
-        for flat_idx, red_idx in enumerate(np.ndindex(*correction_dims.shape)):
-            full_idx = self._build_full_index(prm.dims, correction_dims, red_idx, drop_letters)
-            val = prm.values[full_idx].copy()
-
-            # Perform perturbation
-            prm.values[full_idx] = val * (1 + epsilon)
-            f_perturbed = f(self.prms)
-
-            # Calculate gradient
-            J[:, flat_idx] = self.flatten_fd_to_np(f_perturbed - f0) / epsilon
-
-            # Restore original value
-            prm.values[full_idx] = val
-
-        return J
-    
-    @staticmethod
-    def _build_full_index(prm_dims, correction_dims, red_idx, drop_letters):
-        full_letters = prm_dims.letters
-        red_letters = correction_dims.letters
-        red_axes = dict(zip(red_letters, red_idx))
-        full_idx = []
-        for dl in full_letters:
-            if dl in drop_letters:
-                full_idx.append(slice(None))
-            else:
-                full_idx.append(red_axes[dl])
-        return tuple(full_idx)
-
-    def pre_compute_lambda(self):
-        """Solve Aλ = b for λ."""
-
-        b = np.log(self.flatten_fd_to_np(self.td)) - np.log(self.flatten_fd_to_np(self.bu))
-        D = b.size
-        A = np.zeros((D, D))
-        for prm_name, S in self.S_matrices.items():
-            var_vec = self.get_sigma(prm_name)
-            S_weighted = S * var_vec[np.newaxis, :]
-            A += S_weighted @S.T
-
-        # solve for lambda
-        self.lmda = np.linalg.solve(A, -b)
     
     def calc_corrections(self):
         self.correction_factors = {}
+        # TODO self.S_matrices.keys() replace this with list of corrected parameters
         for prm_name in self.S_matrices.keys():
             log_correction = self.calc_log_correction(prm_name)
             self.correction_factors[prm_name] = log_correction.apply(np.exp)
-
+    
     def calc_log_correction(self, prm_name: str) -> fd.FlodymArray:
-        var_vec = self.get_sigma(prm_name)
         S = self.S_matrices[prm_name]
         grad = S.T @ self.lmda
+        # TODO prepare sigma vector beforehand
+        var_vec = self.get_sigma(prm_name)
         d = var_vec * grad
-        return self._reshape_vector_to_parameter(prm_name, d)
+        return self.reshape_np_to_fd(d, self.prms_adj_dims[prm_name])
 
-    def correct_parameters(self):
-        for prm_name, c in self.correction_factors.items():
-            self.prms[prm_name] = self.prms[prm_name] * c
-
+    def reshape_np_to_fd(self, flat_arr: np.ndarray, target_dims: fd.DimensionSet) -> fd.FlodymArray:
+        """Reshape a 1D numpy array back into a FlodymArray with the same shape as the template."""
+        if flat_arr.size != target_dims.total_size:
+            raise ValueError("Size of flat array does not match size of template.")
+        reshaped_values = flat_arr.reshape(target_dims.shape)
+        return fd.FlodymArray(dims=target_dims, values=reshaped_values)
+    
+    def system_model(self, prms: dict[str, fd.FlodymArray]) -> fd.FlodymArray:
+        td = self.calc_top_down_stock(prms)
+        bu = self.calc_bottom_up_stock(prms)
+        return td / bu
 
 class DependencyTracker(dict):
     """Dictionary that tracks accessed keys."""
